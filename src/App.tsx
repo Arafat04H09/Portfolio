d244586718efb9e58e05472b92e8e0c47dd96106
Arafat04H09 <91175479+Arafat04H09@users.noreply.github.com>
2025-01-06 22:28:54 -0500

    insights added

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import Hero from './Pages/Hero.tsx';
 import Projects from './Pages/Projects.tsx';
 import Discord from './Pages/Discord.tsx';
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import Experience from './Pages/Experience.tsx';
 
 function App() {
   return (
     <div className="flex flex-col">
       <Analytics />
+      <SpeedInsights />
       <Hero />
       <Discord /> 
       <Projects />
